63a777acfe1e6de58dd64d7f0e85186ac929bd4c
Dipak Ray <dipakroy6453@gmail.com>
2026-08-18 18:42:28 +0530

    Tic Tac Toe

---
+++ Tic-Tac-Toe/script.js
@@ -0,0 +1,76 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".resetbtn");
+let newGameBtn = document.querySelector("#newBtn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+resetBtn.classList.remove("hidereset");
+let winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,7]
+];
+
+
+let turno = true;
+boxes.forEach((box) =>{
+box.addEventListener("click" , () => {
+    console.log("box was clicked");
+    if(turno){
+        box.innerText = "O";
+        turno = false;
+    }
+    else{
+         box.innerText = "X";
+        turno = true;
+    }
+    box.disabled =true; 
+    checkwinner();
+});
+});
+const resetGame = () =>{
+    turno = true;
+    enableboxes();
+    msgContainer.classList.add("hide");
+}
+const disableboxes = () =>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+const enableboxes = () =>{
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+
+    }
+}
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations , Winner is ${winner}`;
+    resetBtn.classList.add("hidereset");
+    msgContainer.classList.remove("hide");
+    
+}
+let checkwinner = () =>{
+    for(let pattern of  winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log("winner",pos1Val);
+                disableboxes();
+                showWinner(pos1Val);
+            }
+        } 
+
+    }
+};
+resetBtn.addEventListener("click",resetGame);
+newGameBtn.addEventListener("click",resetGame);
+
